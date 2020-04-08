1f2a48cda6ffab6fafab85da8bd0370fece928c5
kwameasare <eugenedannyuriel@gmail.com>
2020-04-08 20:24:14 +0000

    working on thread

--- lib/components/dart/comment_element.dart
+++ lib/components/dart/comment_element.dart
@@ -1,13 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutterapp/model/comment_model.dart';
 
 class comment_element extends StatefulWidget {
+  final comment_model comment_mod;
+  comment_element({Key key, this.comment_mod}) : super(key: key);
+
   @override
   _comment_elementState createState() => _comment_elementState();
 }
 
 class _comment_elementState extends State<comment_element> {
+  comment_model model;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Column(
+        children: <Widget>[
+          ListTile(
+            title: Text(model.comment),
+            leading: CircleAvatar(
+              child: Image.asset(model.image),
+            ),
+          ),
+          Visibility(
+            visible: model.has_reply,
+            child: ListTile(
+              title: Text(model.comment),
+              leading: CircleAvatar(
+                child: Image.asset(model.image),
+              ),
+            ),
+          ),
+          Divider(),
+        ],
+      ),
+    );
   }
 }
--- lib/model/comment_model.dart
+++ lib/model/comment_model.dart
@@ -1,9 +1,12 @@
 class comment_model {
   int id;
   String comment;
+  String reply;
   String category;
+  String no_replies;
   String image;
-  String body;
+  String replyImg;
+  bool has_reply;
   bool isliked;
   String ttl;
   String residence;
@@ -11,9 +14,12 @@ class comment_model {
       {this.id,
       this.comment,
       this.residence,
+      this.reply,
+      this.replyImg,
       this.category,
-      this.body,
+      this.has_reply,
       this.ttl,
+      this.no_replies,
       this.isliked,
       this.image});
 }
